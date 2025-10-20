6785c9402b46cfd592ad584ce449087cc377f70d
Mari <84589139+marikuznet@users.noreply.github.com>
2025-10-20 15:14:22 +0300

    feat: Integrate Vue 2 and Bootstrap with Laravel Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [
+            require('autoprefixer'),
+        ],
+    });
