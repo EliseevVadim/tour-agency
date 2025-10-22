7e6c3e3083521b0dce4f397f85bb6bd6669cf935
Mari <84589139+marikuznet@users.noreply.github.com>
2025-10-22 15:12:54 +0300

    feat: Implement travel directions section layout

--- resources/js/client/components/Promotions/TrainingPromo.vue
+++ resources/js/client/components/Promotions/TrainingPromo.vue
@@ -1,5 +1,17 @@
 <template>
-
+    <section class="promo-training-section container">
+        <div class="skewed-background"></div>
+        <div class="promo-image position-relative">
+            <img src="/img/experts-bg.png" alt="Фон" class="expert-background"/>
+            <img src="/img/experts.png" alt="Эксперты" class="expert-png"/>
+        </div>
+        <div class="promo-content">
+            <h2 class="promo-title"><span>Профессиональное</span> обучение</h2>
+            <p class="promo-description">Наше авторское обучение для людей которые хотят работать в сфере туризма на
+                полную или частичную занятость, или путешествовать с огромными скидками.</p>
+            <a href="#" class="btn btn-cta btn-open rounded-3">посмотреть</a>
+        </div>
+    </section>
 </template>
 
 <script>
