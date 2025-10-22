adfbc09c9f230cc121d024baec5e1e8105150ce7
Mari <84589139+marikuznet@users.noreply.github.com>
2025-10-22 12:49:53 +0300

    feat: Implement reviews section layout

--- resources/js/client/components/Reviews/ReviewSlider.vue
+++ resources/js/client/components/Reviews/ReviewSlider.vue
@@ -1,9 +1,91 @@
 <template>
+    <section class="reviews-section container-fluid container-xl">
+        <h2 class="reviews-title text-center">Отзывы клиентов:</h2>
+        <div class="reviews-slider">
+            <ssr-carousel show-arrows :slides-per-page='1'
+                          paginate-by-slide overflow-visible peek-right='7%' gutter='30'
+                          :responsive='responsive'>
+                <template #back-arrow='{ disabled }'>
+                    <span class="reviews-carousel-left-icon" :class="{'disabled': disabled}"></span>
+                </template>
+                <template #next-arrow='{ disabled }'>
+                    <span class="reviews-carousel-right-icon" :class="{'disabled': disabled}"></span>
+                </template>
 
+                <review-card v-for="review in reviews"
+                             :key="review.id"
+                             :name="review.name"
+                             :location="review.location"
+                             :text="review.text"
+                             :background-image="review.backgroundImage"
+                             :profile-image="review.profileImage"/>
+            </ssr-carousel>
+        </div>
+    </section>
 </template>
 
 <script>
 export default {
     name: "ReviewSlider",
+    data() {
+        return {
+            reviews: [
+                {
+                    id: 1,
+                    name: "Нина Александровна",
+                    location: "Грузии",
+                    text: "Спасибо за организацию отличного путешествия которое запомнится на всю жизнь!",
+                    backgroundImage: '/img/review-sample-bg.png',
+                    profileImage: '/img/avatar-sample.png'
+                },
+                {
+                    id: 2,
+                    name: "Нина Александровна",
+                    location: "Италии",
+                    text: "Все было организовано безупречно, гид — профессионал своего дела!",
+                    backgroundImage: '/img/review-sample-bg.png',
+                    profileImage: '/img/avatar-sample.png'
+                },
+                {
+                    id: 3,
+                    name: "Нина Александровна",
+                    location: "Японии",
+                    text: "Невероятные впечатления! Отдельное спасибо за подборку ресторанов.",
+                    backgroundImage: '/img/review-sample-bg.png',
+                    profileImage: '/img/avatar-sample.png'
+                },
+                {
+                    id: 4,
+                    name: "Нина Александровна",
+                    location: "Японии",
+                    text: "Невероятные впечатления! Отдельное спасибо за подборку ресторанов.",
+                    backgroundImage: '/img/review-sample-bg.png',
+                    profileImage: '/img/avatar-sample.png'
+                },
+                {
+                    id: 5,
+                    name: "Нина Александровна",
+                    location: "Японии",
+                    text: "Невероятные впечатления! Отдельное спасибо за подборку ресторанов.",
+                    backgroundImage: '/img/review-sample-bg.png',
+                    profileImage: '/img/avatar-sample.png'
+                }
+            ],
+            responsive: [
+                {
+                    minWidth: 768,
+                    slidesPerPage: 2,
+                },
+                {
+                    minWidth: 1024,
+                    slidesPerPage: 3,
+                },
+                {
+                    minWidth: 1366,
+                    slidesPerPage: 4,
+                }
+            ]
+        }
+    }
 }
 </script>
