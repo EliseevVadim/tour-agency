e80a641483e2e310d3020e985b499d7793c83fe5
Mari <84589139+marikuznet@users.noreply.github.com>
2025-10-23 12:16:56 +0300

    feat: Implement souvenir promo section layout

--- resources/js/client/components/Promotions/SouvenirPromo.vue
+++ resources/js/client/components/Promotions/SouvenirPromo.vue
@@ -1,5 +1,21 @@
 <template>
+    <section class="promo-souvenir position-relative">
+        <div class="juice-background juice-left"></div>
 
+        <div class="promo-content position-relative container">
+            <div class="text-block">
+                <h2>Наш мерч</h2>
+                <h3 class="mb-3">выбери свой любимый</h3>
+                <a href="https://t.me/ваш_telegram_канал" target="_blank" class="btn btn-cta btn-open rounded-3">посмотреть</a>
+            </div>
+
+            <div class="d-flex flex-fill justify-content-end position-relative souvenir-mockup z-1">
+                <img src="/img/merch/merch-cap.png" alt="Merch Cap" class="merch-cap">
+                <img src="/img/merch/merch-tshirt.png" alt="Merch Tshirt" class="merch-tsirt">
+                <img src="/img/merch/merch-case.png" alt="Merch Case" class="merch-case">
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
