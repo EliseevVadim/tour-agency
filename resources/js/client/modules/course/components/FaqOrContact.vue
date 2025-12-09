1db27eebdb9413720efbc2f6ff4c1a74e49613a3
Mari <84589139+marikuznet@users.noreply.github.com>
2025-12-09 18:39:03 +0300

    feat: implement course faq contact layout

--- resources/js/client/modules/course/components/FaqOrContact.vue
+++ resources/js/client/modules/course/components/FaqOrContact.vue
@@ -1,7 +1,24 @@
 <template>
+    <section class="faq-chat-section container container-xl">
+        <h2 class="section-title title-course text-center">ОСТАЛИСЬ ВОПРОСЫ?</h2>
 
+        <div class="chat-content-wrapper">
+            <div class="chat-icon-container">
+                <img class="chat-icon-image" alt="ask Course" src="/img/ask-course.png">
+            </div>
+
+            <div class="chat-right-content">
+                <div class="chat-text-content">
+                    <p class="chat-description">Остались вопросы или нужна помощь?<br><span class="fw-bold">Просто напиши в чате, и мы с радостью
+                    поможем!</span></p>
+                </div>
+
+                <button class="chat-button btn-cta text-uppercase">открыть чат</button>
+            </div>
+        </div>
+    </section>
 </template>
 
-<script setup>
+<script>
 
 </script>
