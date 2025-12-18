1d7dedb814324462c8a665ad0a7eae6b0649c26b
Mari <84589139+marikuznet@users.noreply.github.com>
2025-12-18 21:43:10 +0300

    fix letter

--- resources/views/emails/purchase_success.blade.php
+++ resources/views/emails/purchase_success.blade.php
@@ -59,7 +59,7 @@
 
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
     <tr>
-        <td align="center" style="padding: 20px 0 20px 0;">
+        <td align="center">
             <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 440px;
             border-collapse: collapse; background: url('https://putclub.ru/img/message-bg.jpg');
             background-position:center top; background-size: cover; overflow: hidden">
@@ -70,10 +70,10 @@
                             <tr>
                                 <td align="center" style="padding: 25px;">
                                     <table border="0" cellpadding="0" cellspacing="0">
-                                        <tr>
+                                        <tr style="text-align: center;">
                                             <td align="center" class="logo-circle" width="180px"
                                                 height="180px" style='border-radius: 50%; background: url("https://putclub.ru/img/bg-btn-gradient.jpg");
-                                            background-position:center top; background-size: contain;"'>
+                                                 background-position:center top; background-size: contain;'>
                                                 <img style="display: block; width: 180px; border: 0;" class="logo"
                                                      alt="logo"
                                                      src="https://putclub.ru/img/logo-courses.png">
@@ -119,9 +119,13 @@
 
                             <tr>
                                 <td align="center">
-                                    <a href="{{ $link }}" target="_blank" class="button" style="color: #ffffff!important; text-decoration: none;
+                                    <a href="{{ $link }}" target="_blank" style="color: #ffffff!important; text-decoration: none;
                                     background: url('https://putclub.ru/img/bg-btn-gradient.jpg');
-                                    background-position:center top; background-size: cover;">
+                                    background-position:center top; background-size: cover; display: inline-block;
+                                    color: #ffffff !important; text-decoration: none;
+                                                 font-size: 1.6rem; line-height: 54px; height: 54px; padding: 0 3.5rem;
+                                                  border: none; outline: none; text-align: center; border-radius: 10px;
+                                            '>">
                                         Перейти
                                     </a>
                                 </td>
@@ -130,6 +134,22 @@
                         </table>
                     </td>
                     <td width="20"></td>
+                </tr>
+                <tr height="15">
+                </tr>
+                <tr>
+                    <td></td>
+                    <td align="center"
+                        style="padding: 10px 30px 10px 30px; background-color: rgba(255,255,255,0.9); border-radius: 15px;">
+                        <span style="font-family: Montserrat, sans-serif; font-size: 1rem;">
+                            Не работает кнопка? Перейдите по ссылке ниже или скопируйте и откройте ее в браузере:
+                            <a href="{{ $link }}" target="_blank"
+                               style="text-decoration: underline; font-family: Montserrat, sans-serif; font-size: 1.2rem">
+                                https://putclub.ru/shop
+                            </a>
+                        </span>
+                    </td>
+                    <td></td>
                 </tr>
 
                 <tr>
