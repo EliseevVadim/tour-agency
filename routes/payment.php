6e3a2da1c141ae673760f67903f7225ebba65ca3
Mari <84589139+marikuznet@users.noreply.github.com>
2026-02-23 19:14:38 +0300

    update payment route

--- routes/payment.php
+++ routes/payment.php
@@ -9,7 +9,7 @@ Route::post('/payments/callback', [PaymentController::class, 'callback'])
     ->name('payment.callback');
 
 Route::post('/gc-payments/create', [PaymentController::class, 'cancel'])->name('gc-payment.create');
-Route::get('/payment/success', [\App\Http\Controllers\GetCourseController::class, 'handleSuccessPayment'])
+Route::post('/payment/success', [\App\Http\Controllers\GetCourseController::class, 'handleSuccessPayment'])
     ->name('payment.success');
-Route::get('/payment/failure', [\App\Http\Controllers\GetCourseController::class, 'handleFailurePayment'])
+Route::post('/payment/failure', [\App\Http\Controllers\GetCourseController::class, 'handleFailurePayment'])
     ->name('payment.failure');
