8d61c4bdb9eb1a6018ab51b69537a4ff799d2995
Mari <kkuznetsova.m@gmail.com>
2026-05-14 18:54:58 +0300

    update z-index in header

--- resources/js/client/components/Fragments/Header.vue
+++ resources/js/client/components/Fragments/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="hero-section" :class="{'w-100 position-fixed':isShop}" :style="{'z-index:99999': isShop}">
+    <section class="hero-section" :class="{'w-100 position-fixed':isShop}" :style="isShop ? { zIndex: 99999 } : {}">
         <nav class="hero-top-bar container-xl d-flex justify-content-between align-items-center">
             <div class="hero-top-bar-left gap-4 d-flex">
                 <button class="navbar-toggler border-0" type="button"
